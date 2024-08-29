b70924dd00c31c3152ae443757888689862a6732
unknown <dennis.sarajlic@uconn.edu>
2024-08-29 11:24:06 -0400

    Code to decrypt message two

--- decrypt_message_two.py
+++ decrypt_message_two.py
@@ -5,3 +5,17 @@ encrypted_message = encrypted_file.readline()
 encrypted_file.close()
 
 # Write Code Here
+
+# Create a list to edit from the string
+decrypted_message = list(encrypted_message)
+
+# Set initial positions
+i, j = 1, len(encrypted_message) - 2
+
+# Iterate through the message 
+while i <= j:
+    decrypted_message[i], decrypted_message[j] = decrypted_message[j], decrypted_message[i]
+    i += 2
+    j -= 2
+
+print("".join(decrypted_message))
